bf97b3a9ebcb1fcce5033432e574f2c2583e0419
Jeff Lo <jeff@ve12sus.com>
2017-12-15 18:07:26 -0500

    makeActive.js - give active classes to subnav

---
+++ scripts/makeActive.js
@@ -0,0 +1,26 @@
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return decodeURI(results[1]) || 0;
+    }
+}
+
+function makeActive(param) {
+  if (param == "Local") {
+    $(".Local-nav-link").addClass("active")
+  } else if (param == 'National') {
+    $(".National-nav-link").addClass("active")
+  } else {
+    $(".news-nav-link").addClass("active");
+    $(".people-nav-link").addClass("active");
+    $(".events-nav-link").addClass("active");
+    $(".food-nav-link").addClass("active");
+  }
+}
+
+var whatisit = $.urlParam('category');
+
+makeActive(whatisit);
